73ca8f76ec1ab31c20927be1fc23cb676d4ac5c7
nairaAbdallah <nairabassam99@gmail.com>
2021-08-25 18:51:42 +0200

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Naira Bassam on 25/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
